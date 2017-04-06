bf28c0eaa9bd53ac002f3d3d30d206d42ea432bc
Clase <Clase>
2017-04-05 19:29:50 -0500

    Commit de Prueba

---
+++ 2013108542/2013108542/Bus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2013108542
+{
+    class Bus : Carro
+    {
+        public numTipoBus TipoBus { get; set; }
+        public Bus()
+            : base()
+        {
+
+        }
+    }
+}
---
+++ 2013108542/2013108542/numTipoAuto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2013108542
+{
+    enum numTipoAuto
+    {
+        Sedan, Pickup, Coupe, HatchBack , NoDefinido
+    }
+}
